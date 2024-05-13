05dc6f35eaaa75a02b660045468d8b96e4ebbca5
Code_with_Fairy <162690320+faryal16@users.noreply.github.com>
2024-05-13 11:52:43 +0500

    updated files

--- index.js
+++ index.js
@@ -1,54 +1,89 @@
 #! /usr/bin/env node
 import inquirer from "inquirer";
+import chalk from "chalk";
+//print a welocme message
+console.log(chalk.magenta.bold("\n\t\t>>>>>>>> WELCOME TO MY PROJECT<<<<<<<\t\n"));
+console.log(chalk.magenta.bold("\t\t~~~~ ATM-Automated_Teller_Machine ~~~~\t\t\n"));
+console.log("~".repeat(50));
 let myBalance = 10000;
 let myPinCode = 12345;
 let pinAns = await inquirer.prompt([
     {
         name: "pin",
-        message: "enter your pin",
+        message: chalk.rgb(255, 102, 204)("Enter your pin"),
         type: "number",
     },
 ]);
 if (pinAns.pin === myPinCode) {
-    console.log("Correct pincode!!!");
+    console.log(chalk.yellowBright("\nCorrect pincode!!!\n"));
     let operationAns = await inquirer.prompt([
         {
             name: "operation",
-            message: "select your operation",
+            message: chalk.rgb(0, 255, 0)("\nSelect your operation\n"),
             type: "list",
             choices: ["withdraw", "checkbalance", "deposite"],
         },
     ]);
     if (operationAns.operation === "withdraw") {
-        let amountAns = await inquirer.prompt([
+        let withdrawAns = await inquirer.prompt([
             {
-                name: "amount",
-                message: "enter your amount",
-                type: "number",
+                name: "withdraw_method",
+                type: "list",
+                message: chalk.rgb(0, 255, 0)("\n Select an operation!!\n "),
+                choices: ["Fast Cash", "Enter Amount"]
             },
         ]);
-        myBalance -= amountAns.amount;
-        if (amountAns.amount <= 10000) {
-            console.log(`your remaining amount is ${myBalance} $`);
+        if (withdrawAns.withdraw_method === "Enter Amount") {
+            let amountAns = await inquirer.prompt([
+                {
+                    name: "amount",
+                    message: chalk.rgb(255, 102, 204)("\nEnter your amount to Withdraw:\n"),
+                    type: "number",
+                },
+            ]);
+            myBalance -= amountAns.amount;
+            if (amountAns.amount <= 10000) {
+                console.log(chalk.green(`\n ${amountAns.amount}$ withdraw successfully!\n`));
+                console.log(chalk.green(`\nyour remaining amount is ${myBalance}$\n`));
+            }
+            else {
+                console.log(chalk.red("\nPlease enter a valid amount you have only 10000\n"));
+            }
         }
-        else {
-            console.log("Please enter a valid amount you have only 10000");
+        else if (withdrawAns.withdraw_method === "Fast Cash") {
+            let fastcashAns = await inquirer.prompt([
+                {
+                    name: "Fast_Cash",
+                    type: "list",
+                    message: chalk.rgb(255, 102, 204)("\nSelect Amount:\n"),
+                    choices: ["500$", "1000$", "1500$", "5000$", "20000$"]
+                }
+            ]);
+            if (fastcashAns.Fast_Cash > myBalance) {
+                console.log(chalk.red.bold("\nInsufficient Balance\n"));
+            }
+            else {
+                myBalance -= fastcashAns.Fast_Cash;
+                console.log(chalk.green(`\n ${fastcashAns.Fast_Cash} withdraw successfully!\n`));
+                console.log(chalk.green(`\nyour remaining amount is ${myBalance}$\n`));
+            }
         }
     }
     else if (operationAns.operation === "checkbalance") {
-        console.log(`your balance is: ${myBalance}$`);
+        console.log(chalk.blue(`\nyour balance is: ${myBalance}$\n`));
     }
     else if (operationAns.operation === "deposite") {
         let cashAns = await inquirer.prompt([
             { name: "cash",
-                message: "which amount you want to deposite",
+                message: chalk.rgb(25, 102, 204)("\nwhich amount you want to deposite?\n"),
                 type: "list",
                 choices: [5000, 10000, 15000, 20000],
             }
         ]);
-        console.log(`today you deposite ${cashAns.cash} cash in your bank account so now your total amount is ${myBalance + cashAns.cash}`);
+        console.log(chalk.green(`\ntoday you deposite ${cashAns.cash} cash in your bank account so now your total amount is ${myBalance + cashAns.cash}\n`));
     }
 }
 else {
-    console.log("INcorrect Pin!!");
+    console.log(chalk.redBright.bold("INcorrect Pin!!"));
 }
+console.log("~".repeat(50));
